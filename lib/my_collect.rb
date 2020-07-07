226cb3a3e5c9e25be9fa3f117296f92fc4ef0da9
Wick Bushong <wickbushong@gmail.com>
2020-07-07 20:47:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(arg)
+  if block_given?
+  i = 0
+  collection = []
+    while i < arg.length
+      collection << yield(arg[i])
+      i += 1
+    end
+    collection
+  end
+end
