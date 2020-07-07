72a62fa2e4f271afbc858cf7c4086f8bca2678b6
Wick Bushong <wickbushong@gmail.com>
2020-07-07 20:45:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(arg)
   if block_given?
   i = 0
-  while i < arg.length
-    collection << yield(arg[i])
-    i += 1
+    while i < arg.length
+      collection << yield(arg[i])
+      i += 1
+    end
+    collection
   end
-end
-  collection
 end
